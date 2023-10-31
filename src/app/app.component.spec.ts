fb08198eea58fce9cca0526d51c9fb7ed9decb92
Ernesto <ernestosaniel123@gmail.com>
2023-10-31 11:49:19 -0500

    Landing Page Desplegada

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PetsNature_Frontend'`, () => {
+  it(`should have as title 'PetsNature'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PetsNature_Frontend');
+    expect(app.title).toEqual('PetsNature');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PetsNature_Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PetsNature app is running!');
   });
 });
