6c4de9c8f79f7921adf13ea11f117a1a2376fd35
Ernesto <ernestosaniel123@gmail.com>
2023-11-20 01:39:45 -0500

    Intefaz ver perfil completada

--- src/app/intranet/usuario-perfil/usuario-perfil.component.ts
+++ src/app/intranet/usuario-perfil/usuario-perfil.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class UsuarioPerfilComponent {
 
+  imagenPerfil = './assets/img-perfil.png'; // Ruta de la imagen predeterminada
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imagenPerfil = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  subirFoto() {
+    // Implementa la lógica para subir la imagen al servidor si es necesario.
+    // Puedes acceder al archivo seleccionado desde this.imagenPerfil.
+    // Por ejemplo, podrías usar una biblioteca como 'HttpClient' para enviar la imagen al servidor.
+    console.log('Implementa la lógica para subir la foto al servidor aquí.');
+  }
 }
