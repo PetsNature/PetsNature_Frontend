369164b1c3709f6888414c61a74a514c1ca394ba
Jose Allain <joseallain27@gmail.com>
2023-11-27 17:44:51 -0500

    4 historias integradas

--- src/@api/publicaciones-api.service.ts
+++ src/@api/publicaciones-api.service.ts
@@ -1,6 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { lastValueFrom} from 'rxjs';
+import {Time} from "@angular/common";
+
+/*export interface Publicacion1 {
+  id: string;
+  categoria: string;
+  tema: string;
+  tipoMascota: string;
+  raza: string;
+  contenido: string;
+  e_interes: string;
+  fecha: string;
+  fecha_creacion: string;
+  comentarios: any[];
+  likes: any[];
+  imagen: any;
+}*/
 
 export interface Publicacion {
   id: string;
@@ -14,11 +30,43 @@ export interface Publicacion {
   fecha_creacion: string;
   comentarios: any[];
   likes: any[];
-  imagen: any;
+  imagen2: any;
+}
+
+export interface Tema{
+  tema: String
+}
+
+export interface TipoMascota{
+  nombre: String
+}
+
+export interface RazaAnimal{
+  nombre: String
+  tipoMascota: TipoMascota
+}
+
+export interface CrearPublicacion {
+  tema: Tema;
+  tipoMascota: TipoMascota;
+  razaAnimal: RazaAnimal;
+  contenido: string;
+  categoria: string;
+  enlace: string;
 }
 
 export interface SavePublicacionResponse{
-  message: string;
+   date: Date;
+   hora: Time;
+   categoria: string;
+   id_usuario: number;
+   tema: Tema;
+   tipoMascota: TipoMascota;
+   enlace: string;
+   contenido: string;
+   img: string;
+   reacciones: bigint;
+   razaAnimal: RazaAnimal;
 }
 
 @Injectable({
@@ -28,15 +76,10 @@ export interface SavePublicacionResponse{
 export class PublicacionesApiService {
 
   httpClient = inject(HttpClient)
-  url="localhost:8080/descubre/{id}/crear_publicacion"
 
-  getListPublicaciones(){
-    return lastValueFrom(this.httpClient.get<Publicacion[]>(this.url));
+
+  async crearPublicacionSinImg(formData: FormData, id: number) {
+    console.log(formData.get('crearPublicacionSerializer'))
+    return lastValueFrom(this.httpClient.post<SavePublicacionResponse>(`http://localhost:8080/descubre/${id}/crear_publicacion`,formData,{ headers:{'No-Token': 'true'}}));
   }
-
-  async savePublicacion(publicacion: Publicacion){
-    return lastValueFrom(this.httpClient.post<SavePublicacionResponse>(this.url, publicacion));
-  }
-
 }
-
--- src/app/intranet/comunidad/publicaciones-usuario/crear-publicacion/crear-publicacion.component.ts
+++ src/app/intranet/comunidad/publicaciones-usuario/crear-publicacion/crear-publicacion.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { PublicacionService } from '../../publicacion.service';
+import {
+  CrearPublicacion,
+  PublicacionesApiService,
+  RazaAnimal,
+  Tema,
+  TipoMascota
+} from "../../../../../@api/publicaciones-api.service";
+import {User} from "../../../../authentication/authentication.service";
+import {LocalStorageSubject} from "../../../../../shared/localstorage/localStorageSubject";
+import {ERROR} from "@angular/compiler-cli/src/ngtsc/logging/src/console_logger";
 
 @Component({
   selector: 'app-crear-publicacion',
@@ -8,7 +18,7 @@ import { PublicacionService } from '../../publicacion.service';
 })
 
 export class CrearPublicacionComponent implements OnInit {
- 
+
   categoria: string = 'informacion'; //por defecto es informacion
   tema: string= 'No especificado';
   tipoMascota: string = 'No especificado';
@@ -17,15 +27,39 @@ export class CrearPublicacionComponent implements OnInit {
   contenido: string = '';
   mostrarAdvertenciaContenido: boolean = false;
   e_interes: string ='';
-  selectedFile: File = new File([], '');
+  selectedFile: File = new File([],'');
   publicacionActual: any;
+
+  tema2: Tema={
+    tema:'No especificado',
+  }
+
+  tipoMascota2:TipoMascota={
+    nombre:'No especificado'
+  }
+
+  raza2:RazaAnimal={
+    nombre:'Poodle',
+    tipoMascota:this.tipoMascota2
+  }
+
+  publicacion2:CrearPublicacion={
+    tema: this.tema2,
+    tipoMascota: this.tipoMascota2,
+    razaAnimal: this.raza2,
+    contenido: '',
+    categoria: 'informacion',
+    enlace: '',
+  }
+
+  apiPublicacion=inject(PublicacionesApiService)
 
   constructor(private publicacionService: PublicacionService) { }
 
   ngOnInit() {
     this.publicacionActual = this.publicacionService.getPublicacionActual();
   }
-  
+
 
   razasPorTipo: { [key: string]: string[] } = {
     "Perros": ["Labrador Retriever", "Shih Tzu","Bulldog Francés", "Pastor Alemán", "Golden Retriever", "Poodle", "Dachshund"],
@@ -40,10 +74,14 @@ export class CrearPublicacionComponent implements OnInit {
     let fecha = new Date();
     return fecha.getTime().toString();
   }
-  
+
 
   setCategoria(nuevaCategoria: string): void {
     this.categoria = nuevaCategoria;
+  }
+
+  setCategoria2(nuevaCategoria: string): void {
+    this.publicacion2.categoria = nuevaCategoria;
   }
 
   getButtonStyles(categoria: string): any {
@@ -59,14 +97,14 @@ export class CrearPublicacionComponent implements OnInit {
     this.raza= 'No especificada';
     this.contenido= '';
     this.e_interes= '';
-    
+
     // Restablecer la imagen de vista previa
     let previewElement = <HTMLImageElement>document.getElementById('preview');
     if (previewElement) {
       previewElement.src = '';
     }
   }
-  
+
 
   actualizarRazas() {
     this.razas = this.razasPorTipo[this.tipoMascota] || [];
@@ -78,17 +116,17 @@ export class CrearPublicacionComponent implements OnInit {
     publicaciones.push(publicacion);
     localStorage.setItem('publicaciones', JSON.stringify(publicaciones));
   }
-  
+
   cargarPublicaciones(): any[] {
     let publicaciones = localStorage.getItem('publicaciones');
     if (publicaciones !== null) {
       return JSON.parse(publicaciones);
     } else {
-    
+
       return [];
     }
   }
-  
+
   onFileSelected(event: Event) {
     let inputElement = <HTMLInputElement>event.target;
     if (inputElement.files && inputElement.files.length > 0) {
@@ -104,25 +142,33 @@ export class CrearPublicacionComponent implements OnInit {
     }
   }
 
+  /*onFileSelected2(event: Event) {
+    const formData=new FormData()
+    if (this.publicacion2.imagenxd){
+      formData.append('file',this.publicacion2.imagenxd)
+    }
+
+  }*/
+
   triggerFileInput() {
     let fileInput = document.getElementById('fileInput');
     if (fileInput) {
       fileInput.click();
     }
   }
-  
+
   publicar() {
     if (this.contenido) {
       this.mostrarAdvertenciaContenido = false;
-      
+
       // Obtenemos la fecha actual
       let fechaActual = new Date();
-      
+
       // Formateamos la fecha en el formato DD/MM/AA
       let fechaFormateada = ('0' + fechaActual.getDate()).slice(-2) + '/'
                          + ('0' + (fechaActual.getMonth()+1)).slice(-2) + '/'
                          + fechaActual.getFullYear().toString().substr(-2);
-      
+
       let nuevaPublicacion = {
         id: this.generarId(),
         categoria: this.categoria,
@@ -132,12 +178,12 @@ export class CrearPublicacionComponent implements OnInit {
         contenido: this.contenido,
         e_interes: this.e_interes,
         fecha: fechaFormateada,  // Agregamos la fecha a la publicación
-        fecha_creacion: fechaActual, 
+        fecha_creacion: fechaActual,
         comentarios: [],
         likes: 0,  // Aquí se almacenarán los "likes"
-        imagen: null, 
+        imagen: null,
       };
-  
+
       if (this.selectedFile.size > 0) {  // Verifica si se ha seleccionado un archivo
         let reader = new FileReader();
         reader.onload = (event: any) => {
@@ -158,6 +204,65 @@ export class CrearPublicacionComponent implements OnInit {
       this.mostrarAdvertenciaContenido = true;
     }
   }
-  
-  
+
+  getIdCliente():number | null{
+    const user=localStorage.getItem("AUTH_USER")
+    if (user !==null){
+      let usuario:User =JSON.parse(user);
+      return usuario.id
+    }
+    alert("Debe Iniciar Sesion para realizar esta accion")
+    return null
+  }
+
+  async publicar2() {
+    if (this.publicacion2.contenido) {
+      this.mostrarAdvertenciaContenido = false;
+      const idCliente = this.getIdCliente()
+      if (idCliente !== null) {
+        const formData=new FormData()
+        formData.append('publicacion',JSON.stringify(this.publicacion2))
+        if (this.selectedFile.size > 0) {
+          formData.append('imagen',this.selectedFile)// Verifica si se ha seleccionado un archivo
+            await this.apiPublicacion.crearPublicacionSinImg(formData, idCliente)
+            this.limpiar_pub();
+            console.log('Publicación creada:', this.publicacion2);
+        } else {  // Si no se ha seleccionado un archivo, guarda la publicación sin imagen
+          await this.apiPublicacion.crearPublicacionSinImg(formData, idCliente)
+          this.limpiar_pub();
+          console.log('Publicación creada:', this.publicacion2);
+        }
+      }
+    } else {
+      this.mostrarAdvertenciaContenido = true;
+    }
+  }
+
+  /*async publicar2() {
+    if (this.crearPublicacionSerializer.contenido) {
+      this.mostrarAdvertenciaContenido = false;
+      const idCliente = this.getIdCliente()
+      if (idCliente !== null) {
+        const formData=new FormData()
+        const jsonBlob = new Blob([JSON.stringify(this.crearPublicacionSerializer)], { type: 'application/json' });
+        formData.append('crearPublicacionSerializer', jsonBlob, 'datos.json');
+        if (this.selectedFile.size > 0) {
+          formData.append('imagen',this.selectedFile)// Verifica si se ha seleccionado un archivo
+          await this.apiPublicacion.crearPublicacionSinImg(formData, idCliente)
+          this.limpiar_pub();
+          console.log('Publicación creada:', this.crearPublicacionSerializer);
+        } else {  // Si no se ha seleccionado un archivo, guarda la publicación sin imagen
+          await this.apiPublicacion.crearPublicacionSinImg(formData, idCliente)
+          this.limpiar_pub();
+          console.log('Publicación creada:', this.crearPublicacionSerializer);
+        }
+      }
+    } else {
+      this.mostrarAdvertenciaContenido = true;
+    }*/
+  handleFileInput(event: any) {
+    //const file = event.target.files[0];
+    this.selectedFile = event.target.files[0];
+  }
 }
+
