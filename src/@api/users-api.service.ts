84bc1c79e393a9ff72ad4f4d8d28bfbd619fafb9
Jose Allain <joseallain27@gmail.com>
2023-12-07 22:46:18 -0500

    New IP

--- src/@api/users-api.service.ts
+++ src/@api/users-api.service.ts
@@ -28,10 +28,10 @@ export class UsersApiService {
   httpClient = inject(HttpClient)
 
   async registerUser(user:UsuarioRegistro){
-      return lastValueFrom(this.httpClient.post<Usuario[]>("http://3.146.35.150/registro",user,{ headers:{'No-Token': 'true' }}));
+      return lastValueFrom(this.httpClient.post<Usuario[]>("http://18.119.162.163/registro",user,{ headers:{'No-Token': 'true' }}));
   }
 
   async loginUser(user: UsuarioLogin){
-    return lastValueFrom(this.httpClient.post<Usuario>("http://3.146.35.150/login2", user,{ headers:{'No-Token': 'true' }}));
+    return lastValueFrom(this.httpClient.post<Usuario>("http://18.119.162.163/login2", user,{ headers:{'No-Token': 'true' }}));
   }
 }
